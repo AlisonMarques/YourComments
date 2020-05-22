1fa5ae9faf43e4eb541bfab8c5d7aa915f7d2038
Alison Marques <alison-marques@outlook.com.br>
2020-05-22 16:27:32 -0300

    some validations on the routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const { celebrate, Segments, Joi } = require('celebrate');
+
 const UserController = require('./controllers/UserController');
 const PersoDataController = require('./controllers/PersoDataController');
 const ProfileController = require('./controllers/ProfileController');
@@ -10,12 +12,50 @@ const routes = express.Router();
 routes.post('/sessions', SessionController.create);
 
 routes.get('/profiles', UserController.index);
-routes.post('/profiles', UserController.create);
 
-routes.get('/profile', ProfileController.index);
+routes.post(
+  '/profiles',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required().min(3),
+      email: Joi.string().required().email(),
+      age: Joi.string().required().max(2),
+      cpf: Joi.string().required().max(11),
+      rg: Joi.string().required().max(7),
+    }),
+  }),
+  UserController.create
+);
 
-routes.get('/comments', PersoDataController.index);
+routes.get(
+  '/profile',
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+  }),
+  ProfileController.index
+);
+
+routes.get(
+  '/comments',
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number(),
+    }),
+  }),
+  PersoDataController.index
+);
 routes.post('/comments', PersoDataController.create);
-routes.delete('/comments/:id', PersoDataController.delete);
+
+routes.delete(
+  '/comments/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required(),
+    }),
+  }),
+  PersoDataController.delete
+);
 
 module.exports = routes;
